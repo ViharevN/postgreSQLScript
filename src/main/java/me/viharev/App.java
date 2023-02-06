77eefaba754b24407e6b2ff3e54f0a92a7a7c63c
ViharevN <swix_off@mail.ru>
2023-02-06 22:52:43 +0300

    commit

---
+++ src/main/java/me/viharev/App.java
@@ -0,0 +1,13 @@
+package me.viharev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
