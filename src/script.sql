ba052a0c3cc51a38bad85b6d8ac3c101d22c27aa
ViharevN <swix_off@mail.ru>
2023-02-08 01:51:18 +0300

    задание 2

--- src/script.sql
+++ src/script.sql
@@ -16,3 +16,78 @@ update employee set first_name='Ann', last_name='Popova',gender='woman',age=18 w
 select * from employee;
 delete from employee where id=1;
 select * from employee;
+
+-- домашняя работа 2
+-- Задание 1
+
+
+insert into employee (first_name, last_name, gender, age)
+values ('Bob','Bobov','man',19);
+insert into employee (first_name, last_name, gender, age)
+values ('Viktor','Viktorov','man',20);
+insert into employee (first_name, last_name, gender, age)
+VALUES ('Vera','Sergeevna','woman',27);
+select * from employee;
+
+select first_name AS Имя,
+       last_name AS Фамилия
+from employee group by Имя, Фамилия;
+
+select last_name AS Фамилия,
+       age AS Возраст
+from employee
+where age < 30
+order by Возраст desc, Фамилия;
+
+select last_name AS Фамилия,
+       age AS Возраст
+from employee
+where age > 50
+order by Возраст desc, Фамилия;
+
+select last_name AS Фамилия,
+       age AS Возраст
+from employee
+where age > 30 AND age < 50
+order by Возраст desc, Фамилия;
+
+select first_name AS Имя,
+       last_name AS Фамилия,
+       age AS Возраст
+from employee
+where age > 18 AND age < 30
+order by Возраст desc, Имя, Фамилия;
+
+select last_name AS Фамилия,
+       id AS Номер
+from employee
+order by id desc;
+
+select first_name AS Имя
+from employee group by first_name
+HAVING length(first_name)>4;
+
+select last_name AS Фамилия
+from employee group by last_name
+HAVING length(last_name)<7;
+
+-- Задание 2
+update employee set first_name='Ivan', last_name='Ivanov' where id=2;
+update employee set first_name='Ivan', last_name='Ivanov' where id=4;
+
+select first_name AS Имя,
+       sum(age) AS Возраст_Суммарный
+from employee group by first_name;
+
+select * from employee
+where age=(
+    select min(age)
+    from employee
+);
+
+select first_name AS Имя,
+       max(age) AS Максимальный_Возраст
+from employee GROUP BY Имя
+HAVING count(first_name)>1
+order by Максимальный_Возраст;
+
